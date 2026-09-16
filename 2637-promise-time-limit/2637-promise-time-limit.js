492881de733b29422ff64c25c5f9c4e183b2874c
Surendra <surendrachennamalli177@gmail.com>
2026-09-16 12:04:43 +0530

    Time: 50 ms (55.2%), Space: 53.8 MB (67.74%) - LeetHub

---
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -0,0 +1,12 @@
+var timeLimit = function(fn, t) {
+    return async function(...args) {
+        return Promise.race([
+            fn(...args),
+            new Promise((_, reject) => {
+                setTimeout(() => {
+                    reject("Time Limit Exceeded");
+                }, t);
+            })
+        ]);
+    };
+};
